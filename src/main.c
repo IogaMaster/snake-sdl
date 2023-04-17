69c7a2d39aa61dd5665d2945b74d8eb28a060710
IogaMaster <iogamastercode@gmail.com>
2023-04-17 00:07:43 -0600

    Formatting

--- src/main.c
+++ src/main.c
@@ -3,15 +3,10 @@
 #include <stdio.h>
 
 int main() {
-  // The window we'll be rendering to
   SDL_Window *window =
       SDL_CreateWindow("Snake SDL", 0, 0, 1080, 720, SDL_WINDOW_SHOWN);
-
-  // The surface contained by the window
   SDL_Surface *surface = SDL_GetWindowSurface(window);
-
   SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, 0xFF, 0xFF, 0xFF));
-
   SDL_UpdateWindowSurface(window);
 
   SDL_Event e;
@@ -22,8 +17,6 @@ int main() {
         quit = true;
     }
   }
-
-  // Cleanup
   SDL_DestroyWindow(window);
   SDL_Quit();
 
